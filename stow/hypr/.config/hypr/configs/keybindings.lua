86ee77c8b6135f031cea8fde9e02fb1ab156d402
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2026-06-12 12:45:06 +0200

    Fix keybindings

--- stow/hypr/.config/hypr/configs/keybindings.lua
+++ stow/hypr/.config/hypr/configs/keybindings.lua
@@ -5,7 +5,7 @@ local menu        = "hyprlauncher"
 
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd("killactive"))
+hl.bind(mainMod .. " + C", hl.dsp.window.close())
 
 
 -- Switch workspaces with mainMod + [0-9]
@@ -19,14 +19,14 @@ end
 hl.bind(mainMod .. " + P", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit"))
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd("hyprlauncher"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + V", hl.dsp.toggle_floating())
+hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd("google-chrome-stable --enable-features=UseOzonePlatform,VaapiVideoDecodeLinuxGL,VaapiVideoEncoder --ozone-platform=wayland --use-gl=egl --enable-gpu-rasterization --ignore-gpu-blocklist"))
-hl.bind(mainMod .. " + F", hl.dsp.fullscreen(0))
-hl.bind(mainMod .. " + M", hl.dsp.fullscreen(1))
+-- hl.bind(mainMod .. " + F", hl.dsp.fullscreen(0))
+-- hl.bind(mainMod .. " + M", hl.dsp.fullscreen(1))
 
-hl.bind(mainMod .. " + H", hl.dsp.move_focus("l"))
-hl.bind(mainMod .. " + L", hl.dsp.move_focus("r"))
-hl.bind(mainMod .. " + K", hl.dsp.move_focus("u"))
-hl.bind(mainMod .. " + J", hl.dsp.move_focus("d"))
-hl.bind(mainMod .. " + W", hl.dsp.exec_cmd("sh -c 'pkill -USR1 ashell'"))
+-- hl.bind(mainMod .. " + H", hl.dsp.move_focus("l"))
+-- hl.bind(mainMod .. " + L", hl.dsp.move_focus("r"))
+-- hl.bind(mainMod .. " + K", hl.dsp.move_focus("u"))
+-- hl.bind(mainMod .. " + J", hl.dsp.move_focus("d"))
+-- hl.bind(mainMod .. " + W", hl.dsp.exec_cmd("sh -c 'pkill -USR1 ashell'"))
--- stow/hypr/.config/hypr/configs/monitors.lua
+++ stow/hypr/.config/hypr/configs/monitors.lua
@@ -1,5 +1,5 @@
 hl.monitor({
-  output = "DP-1",
+  output = "eDP-1",
   mode = "1920x1080@144",
   position = "0x0",
   scale = 1,
